366be0543c026862815ba6ac7f192cd4717fad9f
“scottasc” <scottschulfer@gmail.com>
2018-08-30 21:31:54 -0500

    classwork 8/30

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   namespace :api do 
     get "/recipes" => 'recipes#index'
     get "/recipes/:id" => 'recipes#show'
+    post "/recipes" => 'recipes#create'
+    patch "/recipes/:id" => 'recipes#update'
+    delete "/recipes/:id" => 'recipes#destroy'
   end
 end
