3946fbb3a6cc20c026d76ab0992d59a3ed182dbf
“scottasc” <scottschulfer@gmail.com>
2018-09-09 16:55:28 -0500

    adding user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do 
+    post "/users" => 'users#create'
+
     get "/recipes" => 'recipes#index'
     get "/recipes/:id" => 'recipes#show'
     post "/recipes" => 'recipes#create'
